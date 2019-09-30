7d3321ffc30883b9996e005f8bab24766eca333f
IgorShchur <mister906090@gmail.com>
2019-09-30 20:12:27 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,142 @@
+// let a = +prompt(1);
+//
+// let b = +prompt(5);
+//
+// let c = +prompt(10);
+//
+// if (a < b && b < c) {
+//     console.log(a, b, c);
+// } else if (a<c && c<b) {
+//     console.log(a, b, c);
+// } else if (b<a && a<c ) {
+//     console.log(a, b, c);
+// } else if (b<c && c<a) {
+//     console.log(a, b, c);
+// } else if (c>a && a<b) {
+//     console.log(a, b, c);
+// } else if (c<b && b<a) {
+//     console.log(a, b, c);
+// }
+
+// const NORMAL_NUMBER = 458;
+//
+// const FIRST_NUMBER = parseInt( NORMAL_NUMBER / 100) ;
+// const SECOND_NUMBER = parseInt ( (NORMAL_NUMBER % 100 )/ 10);
+// const B_NUMBER = parseInt( NORMAL_NUMBER % 10);
+//
+// console.log (B_NUMBER + '' + SECOND_NUMBER + '' + FIRST_NUMBER);
+
+// let arr = [2,17,13,6,22,31,45,66,100,-18];
+// let =
+// for (let i = 0; i < arr.length; i++ ) {
+//     console.log(arr[i]);
+// }
+// //
+// let arr = [2,17,13,6,22,31,45,66,100,-18];
+// let = 0
+// while ()
+//
+// let arr = [];
+//
+//  while (arr.length<50){
+//
+//      let randomNumber = parseInt(Math.random()*999);
+//      if(randomNumber %2===0) {
+//          arr.push (randomNumber);
+//      }
+//  }
+// console.log(arr)
+// console.log(arr.length);
+
+// TODO 27.08
+
+
+// Даны два массива: ['a', 'b', 'c'] и [1, 2, 3]. Объедините их вместе.
+// - Дан массив ['a', 'b', 'c']. Добавьте ему в конец элементы 1, 2, 3.
+// - Дан массив [1, 2, 3]. Сделайте из него массив [3, 2, 1].
+// - Дан массив [1, 2, 3]. Добавьте ему в конец элементы 4, 5, 6.
+// - Дан массив [1, 2, 3]. Добавьте ему в начало элементы 4, 5, 6.
+// - Дан массив ['js', 'css', 'jq']. Выведите на экран первый элемент при помощи shift
+// - Дан массив ['js', 'css', 'jq']. Выведите на экран последний элемент при помощи pop
+// - slice. Дан массив [1, 2, 3, 4, 5]. С помощью метода slice запишите в новый элементы [1, 2, 3].
+// - slice. Дан массив [1, 2, 3, 4, 5]. С помощью метода slice запишите в новый элементы
+
+
+// 1.
+// let  arr1 = ['a','b','c'];
+// let  arr2 = [1,2,3];
+//
+// let arr3 = arr1.concat(arr2);
+// console.log(arr3);
+
+2.
+
+// let  arr1 = ['a','b','c'];
+//
+//  arr1.push('1','2','3');
+// console.log(arr1);
+
+3.
+
+// let  arr1 = ['1','2','3'];
+//
+// arr1.reverse();
+// console.log(arr1);
+//
+ 4.
+
+// let  arr1 = ['1','2','3'];
+// arr1.push('4','5','6');
+// console.log(arr1);
+
+// 5.
+//
+// let  arr1 = ['1','2','3'];
+// arr1.unshift('4','5','6');
+// console.log(arr1);
+
+//let  arr1 = {js:'test', jq: 'hello', css: 'world'};
+// arr1.splice(1,2);
+// console.log(arr1);
+
+
+// let arr = arr1.splice(1,3);
+// console.log(arr);
+//  arr1.splice(3,0,'a','b','c');
+// console.log(arr1);
+// arr1.splice(3,0,'a','b','c');
+// arr1.splice(1,0,'a','b');
+// arr1.splice(6,0,'c');
+// arr1.splice(8,0,'e');
+// console.log(arr1);
+//
+
+
+
+const  myDay = function () {
+    return new promise
+}
+
+
+     function weyk up() {
+         return new promise
+     }
+
+
+
+    promise.then(function*)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
